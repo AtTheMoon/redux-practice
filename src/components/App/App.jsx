5faf4b8e37f3c17adb3afacd318b1407a2f7adc2
Iwasnotgivenanickname <darklikeblackornot@gmail.com>
2022-12-22 12:31:52 +0600

    counter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, dicrement, plusFive, minusFive, resetFunc } from '../../redux/countSlice'
 
 export const App = () => {
-  const data = useSelector((value)=> value )
-  console.log(data);
+  const {count, name} = useSelector((state)=> (state.counter))
+  const dispatch = useDispatch()
+
+  const [number, setNumber] = useState(0)
+  
+
   return (
     <>
-        <h1>{data.counter.count}</h1>
-        <h1>{data.counter.name}</h1>
+        <input 
+          type="text" 
+          value={number} 
+          onChange={(e)=>{setNumber(e.target.value)}}/>
+        <h1>
+          {/* <button disabled={count == 50 && true} onClick={()=>{dispatch(minusFive())}}>-5</button>
+          <button  disabled={count == 0 && true}  onClick={()=>{dispatch(dicrement())}}>-</button> */}
+          {count}, {name} 
+          <button disabled={count == 50 && true} onClick={()=>{dispatch(increment(number))}}>+</button>
+          {/* <button disabled={count >= 46 && true} onClick={()=>{dispatch(plusFive())}}>+5</button>
+          <button onClick={()=>{dispatch(resetFunc())}}>Reset</button> */}
+        </h1>
     </>
   )
 }
--- src/redux/countSlice.jsx
+++ src/redux/countSlice.jsx
@@ -4,15 +4,28 @@ export const countSlice = createSlice({
     name: 'counter',
     initialState: {
         count: 9,
-        name: 'Saitama'
+        name: 'Saitama',
     },
     reducers:{
         increment: function(state, action){
-            state.count = state.count + 1
+            state.count = state.count + Number(action.payload)
+            console.log(action);
+        },
+        dicrement: function(state, action){
+            state.count = state.count - 1
+        },
+        plusFive: function(state, action){
+            state.count = state.count + 5
+        },
+        minusFive: function(state, action){
+            state.count = state.count - 5
+        },
+        resetFunc: function(state, action){
+            state.count = 0
         }
     }
 })
 
-export const {increment} = countSlice.actions
+export const {increment, dicrement, plusFive, minusFive, resetFunc} = countSlice.actions
 
 export default countSlice.reducer
